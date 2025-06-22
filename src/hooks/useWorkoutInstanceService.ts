52e1d20c4875d955947761836d2ad86a119cb654
Charles Sterling <c_sterling76@hotmail.com>
2025-06-22 15:21:49 -0700

    Refactor useWorkoutInstanceService to utilize WorkoutInstanceServiceImpl
    - Integrated useApiService to fetch the API service instance.
    - Updated the service instantiation to return WorkoutInstanceServiceImpl instead of MockWorkoutInstanceService.
    - Enhanced memoization to depend on the apiService for improved performance.

--- src/hooks/useWorkoutInstanceService.ts
+++ src/hooks/useWorkoutInstanceService.ts
@@ -1,11 +1,15 @@
+import { useApiService } from "@/contexts/ServiceContext";
 import { WorkoutInstanceService } from "@/domain/interfaces/services/WorkoutInstanceService";
 import { MockWorkoutInstanceService } from "@/services/workout-instances/MockWorkoutInstanceService";
+import { WorkoutInstanceServiceImpl } from "@/services/workout-instances/WorkoutInstanceServiceImpl";
 import { useMemo } from "react";
 
 export function useWorkoutInstanceService(): WorkoutInstanceService {
+  const apiService = useApiService();
   const workoutInstanceService = useMemo(() => {
     return new MockWorkoutInstanceService();
-  }, []);
+    return new WorkoutInstanceServiceImpl(apiService);
+  }, [apiService]);
 
   return workoutInstanceService;
 }
