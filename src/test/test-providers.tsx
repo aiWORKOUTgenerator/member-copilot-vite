ff63a678cbf040c9f6415423eff9477f1a94b787
Charles Sterling <c_sterling76@hotmail.com>
2025-08-18 15:10:34 -0700

    refactor: remove CombinedProviders from AllTheProviders component for simplified provider structure

--- src/test/test-providers.tsx
+++ src/test/test-providers.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { CombinedProviders } from '../contexts/CombinedProviders';
 
 // Custom provider component that includes all necessary providers
 export const AllTheProviders = ({
@@ -11,9 +10,7 @@ export const AllTheProviders = ({
 }) => {
   return (
     <ClerkProvider publishableKey="pk_test_mock_key_for_testing_only">
-      <BrowserRouter>
-        <CombinedProviders>{children}</CombinedProviders>
-      </BrowserRouter>
+      <BrowserRouter>{children}</BrowserRouter>
     </ClerkProvider>
   );
 };
