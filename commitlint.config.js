a8330fa46974ea5588853f45c8764f7fd1b1201c
Charles Sterling <c_sterling76@hotmail.com>
2025-09-25 20:17:30 -0700

    chore(commitlint): increase header max length from 100 to 120 characters

--- commitlint.config.js
+++ commitlint.config.js
@@ -25,6 +25,6 @@ export default {
     // Subject does not have to avoid a full stop
     'subject-full-stop': [0],
     // Allow a bit longer headers, but still recommend a limit
-    'header-max-length': [1, 'always', 100],
+    'header-max-length': [1, 'always', 120],
   },
 };
